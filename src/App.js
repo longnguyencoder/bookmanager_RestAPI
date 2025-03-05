1a46bd74f39f34074f8a6c06d0fe2a3a1915571a
longnguyencoder <long0398481719@gmail.com>
2025-03-05 22:38:25 +0700

    upadte table

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className='card'>
+        <div className='cart-header'>
+          <h1>
+            Danh sách Sinh Viên
+          </h1>
+        </div>
+        <div className='card-body'>
+          <table className='table table-bordered'>
+            <thead>
+              <tr>
+                <th scope='col'>Mã Sinh Viên</th>
+                <th scope='col'>Họ </th>
+                <th scope='col'>Tên</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <th >Nguyễn</th>
+                <th >A</th>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <th>Nguyễn </th>
+                <th>B</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
